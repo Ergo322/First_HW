7b2598e081aee014347e9c3699380ee51214f5bb
Ergo322 <Shikmen2@mail.ru>
2022-08-21 17:35:13 +0300

    решение задачи 2

---
+++ Задача2/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Введите два числа");
+        int A =Convert.ToInt32(Console.ReadLine());
+        int B =Convert.ToInt32(Console.ReadLine());
+        if(A>B)
+        {
+            Console.Write(A);
+        }
+        else
+        {
+            Console.Write(B);
+        }
+    }
+}
