584b3de2dddfae74d899c3b05b668bbb17e0184d
Ergo322 <Shikmen2@mail.ru>
2022-08-21 17:55:12 +0300

    решение4

---
+++ Задача4/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Введите три числа");
+        int A =Convert.ToInt32(Console.ReadLine());
+        int B =Convert.ToInt32(Console.ReadLine());
+        int C =Convert.ToInt32(Console.ReadLine());
+
+        int max = A;
+
+        if (A > max) max = A;
+        if (B > max) max = B;
+        if (C > max) max = C;
+
+
+        Console.Write ("MAX = ");
+        Console.Write (max);
+    }
+}
