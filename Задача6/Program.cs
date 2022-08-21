8f13bf5c20838d4ce413e4f5e5a79cfc7c4cfd6e
Ergo322 <Shikmen2@mail.ru>
2022-08-21 18:14:18 +0300

    решение6

---
+++ Задача6/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Введите число");
+        int A=Convert.ToInt32(Console.ReadLine());
+        int result = A%2;
+        if(result ==0)
+        {
+            Console.Write(A);
+            Console.Write(" -> ДА");
+        }
+        else
+        {
+            Console.Write(A);
+            Console.Write(" -> НЕТ");
+        }
+    }
+}
