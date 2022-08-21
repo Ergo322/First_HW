ca67b10bfc88bcc77f1f98debed1a14c2bae058b
Ergo322 <Shikmen2@mail.ru>
2022-08-21 19:27:44 +0300

    решение8

---
+++ Задача8/Program.cs
@@ -0,0 +1,19 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.Write("Введите число: ");
+
+        int A = Convert.ToInt32(Console.ReadLine());
+
+        int count = 2;
+
+        while (count <= A)
+        {
+            if (count % 2 == 0)
+                Console.Write(count + ", ");
+            count++;
+        }
+    }
+}
