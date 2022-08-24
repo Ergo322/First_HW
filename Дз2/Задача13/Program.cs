e96cb89e892e0b626ac79d007328933b1513ed54
Ergo322 <Shikmen2@mail.ru>
2022-08-25 01:14:34 +0300

    task15

--- Дз2/Задача13/Program.cs
+++ Дз2/Задача13/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿        void ThirdNum()
+        {
+            Console.Clear();
+            Console.WriteLine("Введите число");
+            int A = Convert.ToInt32(Console.ReadLine());
+            int D = A;
+            if (A > 999)
+            {
+                while (D > 999)
+                {
+                    D = D / 10;
+                }
+                int X4 = D % 10;
+                Console.Write(A + " —> " + X4);
+            }
+            else
+            {
+                if (A > 99)
+                {
+                    int X3 = A % 10;
+                    Console.Write(A + " —> " + X3);
+                }
+                else
+                {
+                    Console.Write(A + " —> Третьей цифры нет");
+                }
+            }
+            ThirdNum();
+        }
